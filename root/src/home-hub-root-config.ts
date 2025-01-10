3e7fff5c292c723621a1c8ca5cb898ce97f9dfd4
Edvaldo S de Matos <edvaldo.santiago@hotmail.com>
2025-01-09 21:33:18 -0300

    Commit 4

--- root/src/home-hub-root-config.ts
+++ root/src/home-hub-root-config.ts
@@ -1,22 +1,18 @@
 import { registerApplication, start, LifeCycles } from "single-spa";
 
-// registerApplication({
-//   name: "@single-spa/welcome",
-//   app: () =>
-//     import(
-//       /* webpackIgnore: true */ // @ts-ignore-next
-//       "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
-//     ),
-//   activeWhen: ["/"],
-// });
+registerApplication({
+  name: "@single-spa/welcome",
+  app: () =>
+    import(
+      /* webpackIgnore: true */ // @ts-ignore-next
+      "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
+    ),
+  activeWhen: ["/"],
+});
 
 registerApplication({
-  name:"@home-hub/hello-world",
-  app: () =>
-      import(
-          /* webpackIgnore: true */
-          "@home-hub/hello-world" as string
-          ) as Promise<LifeCycles>,
+  name: "@home-hub/hello-world",
+  app: () => System.import<LifeCycles>("@home-hub/hello-world"),
   activeWhen: ["/"],
 });
 
